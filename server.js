64cba3ba3b3efc0c6a64c691b58420182932a4c8
Devin King <devink@email.sc.edu>
2026-03-29 22:12:05 -0400

    mongoose

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+mongoose
+  .connect("mongodb+srv://devink_db_user:WmOkAIdM604gy4C2@cluster0.repjbtw.mongodb.net/testdb?appName=Cluster0")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect to mongodb...", err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const schema = new mongoose.Schema({
+  name: String,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const Message = mongoose.model("Message", schema);
+
+async function createMessage() {
+  const message = new Message({
+    name: "Hello World",
+  });
+  const result = await message.save();
+  console.log(result);
+}
+
+createMessage();
