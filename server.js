232188082691ba8db334fefa9d036d4fe12a4240
Devin King <devink@email.sc.edu>
2026-03-29 22:41:15 -0400

    Fix boot order for Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect("mongodb+srv://devink_db_user:WmOkAIdM604gy4C2@cluster0.repjbtw.mongodb.net/testdb?appName=Cluster0")
-  .then(() => console.log("Connected to mongodb..."))
+  .then(() => {
+    console.log("Connected to mongodb...");
+    createMessage();
+  })
   .catch((err) => console.error("could not connect to mongodb...", err));
 
 const schema = new mongoose.Schema({
@@ -22,8 +25,6 @@ async function createMessage() {
   const result = await message.save();
   console.log(result);
 }
-
-createMessage();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
